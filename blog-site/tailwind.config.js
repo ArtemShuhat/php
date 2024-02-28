0592c434bef0a5e7891ecfcb464b308e5eb51964
ArtemShuhat <shuhat.artem2006@gmail.com>
2024-02-28 10:02:21 +0200

    add tailwind cfg

---
+++ blog-site/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php,html"],
+  theme: {
+    extend: {
+      colors: {
+        mainBg: "#EDF2F7",
+        secondBg: "#F3F4F6",
+        greyBut: "#6B7280",
+      },
+    },
+  },
+  plugins: [],
+};
